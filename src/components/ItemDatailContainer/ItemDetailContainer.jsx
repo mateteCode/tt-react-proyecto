042aa7b0b5c0de5554223ea54674893ef905e28c
Matías Lorenzo <matete@gmail.com>
2026-06-18 11:04:42 -0300

    feat: add item detail back button

--- src/components/ItemDatailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDatailContainer/ItemDetailContainer.jsx
@@ -1,10 +1,13 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { ItemDetail } from "../ItemDetail/ItemDetail";
 import { Loader } from "../Loader/Loader";
 import { useProductDetail } from "../../hooks/useProductDetail";
 
+import "./ItemDetailContainer.css";
+
 export const ItemDetailContainer = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
   const { itemDetail, loading } = useProductDetail(id);
 
   if (loading) return <Loader />;
@@ -12,7 +15,12 @@ export const ItemDetailContainer = () => {
 
   return (
     <section>
-      <h1>Detalles del producto</h1>
+      <div className="detail-header">
+        <button className="btn-back" onClick={() => navigate(-1)}>
+          <i className="fa-solid fa-arrow-left"></i> Volver
+        </button>
+        <h1>Detalles del producto</h1>
+      </div>
       <div className="products-container">
         <ItemDetail item={itemDetail} />
       </div>
